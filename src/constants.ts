0f065b7ed94eaf0d016e85d3048d7cf8ded83a9b
Petr Lipatov <lipatov3@gmail.com>
2023-11-23 22:50:21 +0300

    fix: deps updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line no-useless-escape
-const urlRegex =
-  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
 export default urlRegex;
