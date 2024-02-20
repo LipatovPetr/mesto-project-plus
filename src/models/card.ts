eb462a3947c022048962a5d524ec826d89635241
Petr Lipatov <lipatov3@gmail.com>
2024-02-20 15:11:22 +0100

    fix(card): regex export fixed

--- src/models/card.ts
+++ src/models/card.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import urlRegex from '../constants';
+import { urlRegex } from '../constants';
 
 interface Card extends Document {
   name: string;
